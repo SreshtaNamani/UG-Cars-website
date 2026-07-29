d1963d8a73456899acf1908a86d64c9218187ab0
SreshtaNamani <sreshtagopinadh@gmail.com>
2026-07-29 17:19:20 +0530

    Working search bar

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,55 @@
-const mainImage = document.querySelector(".main-image");
-const thumbnails = document.querySelectorAll(".thumbnail-row img");
+const searchBtn = document.getElementById("searchBtn");
 
-thumbnails.forEach((thumb) => {
-    thumb.addEventListener("click", () => {
-        mainImage.src = thumb.src;
+if (searchBtn) {
+
+    searchBtn.addEventListener("click", function () {
+
+        const searchText = document
+            .getElementById("searchInput")
+            .value
+            .toLowerCase();
+
+        const brand = document
+            .getElementById("brandFilter")
+            .value
+            .toLowerCase();
+
+        const fuel = document
+            .getElementById("fuelFilter")
+            .value
+            .toLowerCase();
+
+        const cards = document.querySelectorAll(".car-card");
+
+        cards.forEach(function(card){
+
+            const title = card.querySelector("h3").textContent.toLowerCase();
+
+            const cardBrand = card.dataset.brand;
+
+            const cardFuel = card.dataset.fuel;
+
+            const matchesSearch =
+                title.includes(searchText);
+
+            const matchesBrand =
+                brand === "" || cardBrand === brand;
+
+            const matchesFuel =
+                fuel === "" || cardFuel === fuel;
+
+            if(matchesSearch && matchesBrand && matchesFuel){
+
+                card.style.display = "block";
+
+            }else{
+
+                card.style.display = "none";
+
+            }
+
+        });
+
     });
-});
+
+}
